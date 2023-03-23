84d0a9a716d6ed7eacbc1b8c575b4af360e9c6b0
Koh Jia Xian <kohjx96@gmail.com>
2023-03-23 12:01:15 +0800

    setup npm start, print source string

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export const Greeter = (name: string) => `Hello ${name}`; 
+import * as fs from 'fs';
+import { File } from '@babel/types';
+import { ParseResult , parse } from '@babel/parser';
+
+export const parseString = () : File => {
+    // Parse source.txt
+    return parse("code", {
+        sourceType: "module",
+        plugins: ["typescript"],
+    });
+}
+
+export const getString = () : string => {
+    // Read source.txt
+    const code : string = fs.readFileSync("./source.txt", "utf8");
+    return code;
+}
+
+console.log(getString());
