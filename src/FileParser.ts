c7a7c1b89068b14b08c9b29635ff3539bcf2c5c4
Koh Jia Xian <kohjx96@gmail.com>
2023-03-23 12:47:14 +0800

    add ASTVisitor

---
+++ src/FileParser.ts
@@ -0,0 +1,21 @@
+import { parse } from '@babel/parser';
+import * as babelTypes from '@babel/types';
+import * as fs from 'fs';
+
+const parseString = (codeString : string) : babelTypes.File => {
+    // Parse source.txt
+    return parse(codeString, {
+        sourceType: "module",
+        plugins: ["typescript"],
+    });
+}
+
+export const getString = () : string => {
+    // Read source.txt
+    const code : string = fs.readFileSync("./source1.txt", "utf8");
+    return code;
+}
+
+export const getAST = () : babelTypes.File => {
+    return parseString(getString());
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import * as fs from 'fs';
-import { File } from '@babel/types';
-import { ParseResult , parse } from '@babel/parser';
+import { getString, getAST } from './FileParser';
+import { traverseAST } from './ASTVisitor';
 
-export const parseString = () : File => {
-    // Parse source.txt
-    return parse("code", {
-        sourceType: "module",
-        plugins: ["typescript"],
-    });
-}
-
-export const getString = () : string => {
-    // Read source.txt
-    const code : string = fs.readFileSync("./source.txt", "utf8");
-    return code;
-}
-
-console.log(getString());
+// console.log(getString());
+// console.log(getAST());
+traverseAST(getAST());
